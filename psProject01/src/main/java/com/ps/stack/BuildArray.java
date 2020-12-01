1cbe3dc2efcec9c361b2c23b8beaaf9fe247886b
ejKang <joomn11@gmail.com>
2020-12-01 23:45:19 +0900

    Update BuildArray.java

--- psProject01/src/main/java/com/ps/stack/BuildArray.java
+++ psProject01/src/main/java/com/ps/stack/BuildArray.java
@@ -7,14 +7,15 @@ import java.util.Stack;
 public class BuildArray {
 
     public static void main(String[] args) {
-        int[] arr = {1,3};
+        int[] arr = { 1, 3 };
         List<String> temp = BuildArray.buildArray(arr, 5);
         System.out.println(temp);
     }
+
     public static List<String> buildArray(int[] target, int n) {
         List<String> rst = new ArrayList<>();
         int j = 0;
-        for (int i = 1 ; j < target.length; i++) {
+        for (int i = 1; j < target.length; i++) {
             rst.add("Push");
 
             if (target[j] == i) {
