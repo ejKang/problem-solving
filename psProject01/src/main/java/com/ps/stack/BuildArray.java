0de92734f87d9628ee916779b9ed509a1acaefc6
ejKang <joomn11@gmail.com>
2020-12-01 23:41:10 +0900

    stack - build array
    stack - build array

---
+++ psProject01/src/main/java/com/ps/stack/BuildArray.java
@@ -0,0 +1,28 @@
+package com.ps.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BuildArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1,3};
+        List<String> temp = BuildArray.buildArray(arr, 5);
+        System.out.println(temp);
+    }
+    public static List<String> buildArray(int[] target, int n) {
+        List<String> rst = new ArrayList<>();
+        int j = 0;
+        for (int i = 1 ; j < target.length; i++) {
+            rst.add("Push");
+
+            if (target[j] == i) {
+                j++;
+            } else {
+                rst.add("Pop");
+            }
+        }
+        return rst;
+    }
+}
