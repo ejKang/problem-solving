bf7d263178c7ae7f1d8b1f144b451d104ce80b91
ej <joomn11@gmail.com>
2021-04-15 19:33:59 +0900

    add jpa test code
    add basic entity, repository, service, controller

--- spring-boot-practice/src/main/java/com/joo/springbootpractice/entity/MemberEntity.java
+++ spring-boot-practice/src/main/java/com/joo/springbootpractice/entity/MemberEntity.java
@@ -1,8 +1,34 @@
 package com.joo.springbootpractice.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Entity
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "member")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mbrNo;
+
+    private String id;
+
+    private String name;
+
+    @Builder
+    public MemberEntity(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
 }
