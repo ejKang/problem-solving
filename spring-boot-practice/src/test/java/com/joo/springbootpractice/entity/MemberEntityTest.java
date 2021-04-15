b7815f53745194c3b791b20ab11650dfff4e48f5
ej <joomn11@gmail.com>
2021-04-15 19:35:11 +0900

    add test code of jpa

---
+++ spring-boot-practice/src/test/java/com/joo/springbootpractice/entity/MemberEntityTest.java
@@ -0,0 +1,30 @@
+package com.joo.springbootpractice.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberEntityTest {
+
+	@Test
+	public void getId() {
+		final MemberEntity member = MemberEntity.builder()
+											.id("ffff")
+											.name("Tom")
+											.build();
+		
+		final String id = member.getId();
+		
+		assertEquals("ffff", id);
+	}
+	
+	@Test
+	public void getName() {
+		final MemberEntity member = MemberEntity.builder()
+										.id("ffff")
+										.name("Tom")
+										.build();
+		final String name = member.getName();
+		assertEquals("jTom", name);
+	}
+}
