fc8101a8a76739df47bc3d13cb528bea83f1e6bd
ej <joomn11@gmail.com>
2021-04-20 22:24:57 +0900

    add springboot webservice controller, dto, test

---
+++ springboot-webservice/src/test/java/com/joo/book/springboot/springbootwebservice/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.book.springboot.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
