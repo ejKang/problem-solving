c2fe081103007e6c5e2e073ac69fdfb375674b29
ej <joomn11@gmail.com>
2021-05-12 00:05:43 +0900

    add coin change ps ( top-down )

--- psProject01/src/main/java/com/ps/dp/CoinChange.java
+++ psProject01/src/main/java/com/ps/dp/CoinChange.java
@@ -3,9 +3,32 @@ package com.ps.dp;
 public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
-        int[] rst = new int[coins.length + 1];
-        // rst
-        return 0;
+        if (amount < 1) {
+            return 0;
+        }
+
+        int[] rst = new int[coins.length];
+        return dp(coins, amount, rst);
+    }
+
+    private int dp(int[] coins, int remains, int[] rst) {
+
+        if (remains < 0)
+            return -1;
+        if (remains == 0)
+            return 0;
+        if (rst[remains - 1] != 0)
+            return rst[remains - 1];
+
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int temp = dp(coins, remains - coin, rst);
+            if (temp >= 0 && temp < min) {
+                min = temp + 1; // add 1 -> add self count
+            }
+        }
+        rst[remains - 1] = min == Integer.MAX_VALUE ? -1 : min;
+        return rst[remains - 1];
     }
 
 }
