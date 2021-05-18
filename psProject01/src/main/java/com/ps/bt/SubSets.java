4a98bf911f88483a88901b0580005b6ac8499a35
ej <joomn11@gmail.com>
2021-05-18 17:59:57 +0900

    update bt ps - subset code
    remove global variable and
    add new version of backtracking code

--- psProject01/src/main/java/com/ps/bt/SubSets.java
+++ psProject01/src/main/java/com/ps/bt/SubSets.java
@@ -5,43 +5,38 @@ import java.util.List;
 
 public class SubSets {
 
-    List<List<Integer>> output = new ArrayList<>();
-
-    public static void main(String[] args) {
-        SubSets ss = new SubSets();
-        int[] sss = { 1, 2, 3 };
-        List<List<Integer>> tt = ss.subsets(sss);
-        tt.toString();
-        for (List<Integer> ff : ss.output) {
-            System.out.println(ff.toString());
-        }
-
-    }
-
     public List<List<Integer>> subsets(int[] nums) {
         // input = [1, 2, 3]
-
+    	List<List<Integer>> output = new ArrayList<>();
         int idx = 0;
         List<Integer> list = new ArrayList<>();
-        bt(nums, idx, list);
-        return null;
+        bt(output, nums, idx, list);
+        return output;
     }
 
-    private void bt(int[] nums, int idx, List<Integer> list) {
+    private void bt(List<List<Integer>> output, int[] nums, int idx, List<Integer> list) {
         int len = nums.length;
 
         if (idx == len) {
-
-            output.add(new ArrayList<>(list));
+            output.add(new ArrayList<>(list)); // reference copy 
             return;
         }
 
+        bt(output, nums, idx + 1, list);
+        
         int c = nums[idx];
-
-        bt(nums, idx + 1, list);
         list.add(c);
-        bt(nums, idx + 1, list);
+        bt(output, nums, idx + 1, list);
         int temp = list.size();
         list.remove(temp - 1);
+    }
+    
+    public void bt2(List<List<Integer>> output, int[] nums, int idx, List<Integer> list) {
+        output.add(new ArrayList<>(list));
+        for (int i = idx ; i<nums.length; i++) {
+        	list.add(nums[i]);
+        	bt2(output, nums, idx+1, list);
+        	list.remove(list.size() -1);
+        }
     }
 }
