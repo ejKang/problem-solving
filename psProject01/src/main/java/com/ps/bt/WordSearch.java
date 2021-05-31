920916c13e90c6bba578880fb78db667f84bd77e
ej <joomn11@gmail.com>
2021-05-31 23:21:20 +0900

    add backtraking ps - word search solution

--- psProject01/src/main/java/com/ps/bt/WordSearch.java
+++ psProject01/src/main/java/com/ps/bt/WordSearch.java
@@ -1,9 +1,54 @@
 package com.ps.bt;
 
 public class WordSearch {
+    public static boolean[][] visited;
 
     public boolean exist(char[][] board, String word) {
 
+        boolean rst = false;
+        visited = new boolean[board.length][board[0].length];
+
+        for (int i = 0; i < board.length; i++) {
+
+            for (int j = 0; j < board[0].length; j++) {
+                if ((word.charAt(0) == board[i][j]) && backtracking(board, word, i, j, 0)) {
+                    return true;
+                }
+
+            }
+        }
+
+        return rst;
+    }
+
+    private boolean backtracking(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index)
+                || visited[i][j]) {
+            return false;
+        }
+
+        visited[i][j] = true;
+        if (backtracking(board, word, i - 1, j, index + 1) || backtracking(board, word, i + 1, j, index + 1)
+                || backtracking(board, word, i, j - 1, index + 1) || backtracking(board, word, i, j + 1, index + 1)) {
+            return true;
+        }
+
+        visited[i][j] = false;
         return false;
+    }
+
+    public static void main(String[] args) {
+        WordSearch w = new WordSearch();
+        // String t = "ABCB";
+        // String t = "ABCCED";
+        String t = "SEE";
+        char[][] ww = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
+
+        System.out.println(w.exist(ww, t));
+
     }
 }
