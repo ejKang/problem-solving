bc1a018bcb7f466ee297a95aed2ed9b9765166ac
ej <joomn11@gmail.com>
2021-06-02 00:14:17 +0900

    update backtracking subset solution

--- psProject01/src/main/java/com/ps/bt/SubSets.java
+++ psProject01/src/main/java/com/ps/bt/SubSets.java
@@ -7,7 +7,7 @@ public class SubSets {
 
     public List<List<Integer>> subsets(int[] nums) {
         // input = [1, 2, 3]
-    	List<List<Integer>> output = new ArrayList<>();
+        List<List<Integer>> output = new ArrayList<>();
         int idx = 0;
         List<Integer> list = new ArrayList<>();
         bt(output, nums, idx, list);
@@ -18,25 +18,55 @@ public class SubSets {
         int len = nums.length;
 
         if (idx == len) {
-            output.add(new ArrayList<>(list)); // reference copy 
+            output.add(new ArrayList<>(list)); // reference copy
             return;
         }
 
         bt(output, nums, idx + 1, list);
-        
+
         int c = nums[idx];
         list.add(c);
         bt(output, nums, idx + 1, list);
         int temp = list.size();
         list.remove(temp - 1);
     }
-    
+
     public void bt2(List<List<Integer>> output, int[] nums, int idx, List<Integer> list) {
         output.add(new ArrayList<>(list));
-        for (int i = idx ; i<nums.length; i++) {
-        	list.add(nums[i]);
-        	bt2(output, nums, idx+1, list);
-        	list.remove(list.size() -1);
+        for (int i = idx; i < nums.length; i++) {
+            list.add(nums[i]);
+            bt2(output, nums, idx + 1, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> rst = new ArrayList<>();
+
+        subSet2Backtracking(rst, new ArrayList<Integer>(), nums, 0);
+        return rst;
+    }
+
+    private void subSet2Backtracking(List<List<Integer>> rst, ArrayList<Integer> list, int[] nums, int idx) {
+
+        if (idx > nums.length) {
+            return;
+        }
+        rst.add(new ArrayList<>(list));
+
+        for (int i = idx; i < nums.length; i++) {
+            list.add(nums[i]);
+            subSet2Backtracking(rst, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        SubSets s = new SubSets();
+        int[] i = { 1, 2, 3 };
+        List<List<Integer>> g = s.subsets2(i);
+        for (List<Integer> ii : g) {
+            System.out.println(ii);
         }
     }
 }
