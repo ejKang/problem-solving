80fc39c1b120179a9f4cce70066dc8c8d49097b6
ej <joomn11@gmail.com>
2021-06-04 19:54:01 +0900

    add backtracking ps - permutation solution

--- psProject01/src/main/java/com/ps/bt/Permute.java
+++ psProject01/src/main/java/com/ps/bt/Permute.java
@@ -4,14 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permute {
+    public static void main(String[] args) {
+        Permute per = new Permute();
+        int[] a = { 1, 2 };
+        List<List<Integer>> list = per.permute(a);
+
+        for (List<Integer> ll : list) {
+            System.out.println(ll);
+        }
+    }
+
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> rst = new ArrayList<>();
 
-        bt(rst, new ArrayList<>(), nums);
+        // bt(rst, new ArrayList<>(), nums);
+        backtracking2(rst, nums, new ArrayList<Integer>());
         return rst;
     }
 
-    private void bt(List<List<Integer>> rst, List<Integer> list, int[] nums) {
+    public void backtracking2(List<List<Integer>> rst, int[] nums, List<Integer> list) {
+        if (list.size() == nums.length) {
+            rst.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i : nums) {
+            if (!list.contains(i)) {
+                list.add(i);
+                backtracking2(rst, nums, list);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public void bt(List<List<Integer>> rst, List<Integer> list, int[] nums) {
         if (list.size() == nums.length) {
             rst.add(new ArrayList<>(list));
         } else {
