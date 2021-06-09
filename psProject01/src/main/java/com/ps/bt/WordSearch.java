0e4e915568eb7cecc6c70b595af17ff3b24b7711
ej <joomn11@gmail.com>
2021-06-09 23:59:25 +0900

    add backtracking ps - word search

--- psProject01/src/main/java/com/ps/bt/WordSearch.java
+++ psProject01/src/main/java/com/ps/bt/WordSearch.java
@@ -43,12 +43,65 @@ public class WordSearch {
 
     public static void main(String[] args) {
         WordSearch w = new WordSearch();
-        // String t = "ABCB";
-        // String t = "ABCCED";
-        String t = "SEE";
+        // String t = "ABCB"; // false
+        // String t = "ABCCED"; // true
+        String t = "SEE"; // true
         char[][] ww = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
 
-        System.out.println(w.exist(ww, t));
+        System.out.println(w.exist1(ww, t));
 
+    }
+
+    public boolean exist1(char[][] board, String word) {
+
+        char start = word.charAt(0);
+        boolean[][] visited1 = new boolean[board.length][board[0].length];
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (board[i][j] == start) {
+                    if (bt(board, i, j, word, 0, visited1)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean bt(char[][] board, int y, int x, String word, int idx, boolean[][] visited1) {
+
+        if (idx == word.length()) {
+            return true;
+        }
+
+        int i = board.length;
+        int j = board[0].length;
+        if (x < 0 || x >= j) {
+            return false;
+        }
+        if (y < 0 || y >= i) {
+            return false;
+        }
+
+        if (visited1[y][x]) {
+            return false;
+        }
+
+        if (word.charAt(idx) == board[y][x]) {
+            visited1[y][x] = true;
+
+            boolean rst = bt(board, y - 1, x, word, idx + 1, visited1) || bt(board, y + 1, x, word, idx + 1, visited1)
+                    || bt(board, y, x - 1, word, idx + 1, visited1) || bt(board, y, x + 1, word, idx + 1, visited1);
+
+            if (!rst) {
+                visited1[y][x] = false;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
