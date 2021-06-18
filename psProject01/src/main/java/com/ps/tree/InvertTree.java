db17d32f686adbedef70d633badae5149aa15e64
ej <joomn11@gmail.com>
2021-06-18 22:56:38 +0900

    add tree invertTree 226 ps

---
+++ psProject01/src/main/java/com/ps/tree/InvertTree.java
@@ -0,0 +1,49 @@
+package com.ps.tree;
+
+import java.util.Objects;
+import java.util.Stack;
+
+import com.ps.base.TreeNode;
+
+public class InvertTree {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (Objects.isNull(node)) {
+                continue;
+            }
+            TreeNode temp = node.right;
+            node.right = node.left;
+            node.left = temp;
+
+            stack.push(node.right);
+            stack.push(node.left);
+        }
+
+        return root;
+    }
+
+    public TreeNode invertTreeRecursive(TreeNode root) {
+
+        recursive(root);
+        return root;
+    }
+
+    private void recursive(TreeNode node) {
+        if (Objects.isNull(node)) {
+            return;
+        }
+
+        TreeNode tmp = node.right;
+        node.right = node.left;
+        node.left = tmp;
+
+        recursive(node.right);
+        recursive(node.left);
+    }
+}
