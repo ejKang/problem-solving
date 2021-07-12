d62c82c1c9d1367e8b32e6f077e333d4eecd3bfa
ej <joomn11@gmail.com>
2021-07-12 23:54:46 +0900

    add path sum - tree 113 (+backtracking)

--- psProject01/src/main/java/com/ps/tree/PathSum2_113.java
+++ psProject01/src/main/java/com/ps/tree/PathSum2_113.java
@@ -3,6 +3,7 @@ package com.ps.tree;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Stack;
 
 import com.ps.base.TreeNode;
 
@@ -45,5 +46,34 @@ public class PathSum2_113 {
 
         }
         list.remove(list.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum_2(TreeNode root, int targetSum) {
+
+        List<List<Integer>> rst = new ArrayList<>();
+
+        treeBacktracking(rst, new ArrayList<>(), root, targetSum);
+
+        return rst;
+    }
+
+    private void treeBacktracking(List<List<Integer>> rst, List<Integer> list, TreeNode node, int targetSum) {
+
+        if (Objects.isNull(node)) {
+            return;
+        }
+
+        list.add(node.val);
+
+        if (targetSum == node.val && Objects.isNull(node.right) && Objects.isNull(node.left)) {
+            rst.add(new ArrayList<>(list));
+
+        } else {
+            treeBacktracking(rst, list, node.right, targetSum - node.val);
+            treeBacktracking(rst, list, node.left, targetSum - node.val);
+        }
+
+        list.remove(list.size() - 1);
+
     }
 }
