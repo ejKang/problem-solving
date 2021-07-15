3a28c6d9a60a69dc3532a81393a28ec8262ea5f9
ej <joomn11@gmail.com>
2021-07-15 23:21:15 +0900

    update tree diameter - 543 ps

--- psProject01/src/main/java/com/ps/tree/DiameterOfBT_543.java
+++ psProject01/src/main/java/com/ps/tree/DiameterOfBT_543.java
@@ -1,5 +1,7 @@
 package com.ps.tree;
 
+import java.util.Objects;
+
 import com.ps.base.TreeNode;
 
 public class DiameterOfBT_543 {
@@ -22,6 +24,27 @@ public class DiameterOfBT_543 {
         int left = tree(node.left);
 
         max = Math.max(right + left, max);
+
+        return Math.max(right, left) + 1;
+    }
+
+    public int diameterOfBinaryTree_2(TreeNode root) {
+
+        int rootMaxDept = diaTree(root);
+        System.out.println(rootMaxDept);
+        return max;
+    }
+
+    private int diaTree(TreeNode node) {
+
+        if (Objects.isNull(node)) {
+            return 0;
+        }
+
+        int right = diaTree(node.right);
+        int left = diaTree(node.left);
+
+        max = Math.max(max, right + left);
 
         return Math.max(right, left) + 1;
     }
