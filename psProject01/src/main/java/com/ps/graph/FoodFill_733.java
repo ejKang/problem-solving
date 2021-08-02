5abe3f457ed93923af1f581025cc5340b31b3822
ej <joomn11@gmail.com>
2021-08-02 22:46:00 +0900

    update flood fill 733

--- psProject01/src/main/java/com/ps/graph/FoodFill_733.java
+++ psProject01/src/main/java/com/ps/graph/FoodFill_733.java
@@ -4,15 +4,36 @@ public class FoodFill_733 {
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
 
-        boolean[][] visited = new boolean[image.length][image[0].length];
+        // boolean[][] visited = new boolean[image.length][image[0].length];
 
         int oldColor = image[sr][sc];
-        // image[sr][sc] = newColor;
-        dfs(image, visited, sr, sc, newColor, oldColor);
+
+        // dfs(image, visited, sr, sc, newColor, oldColor);
+        if (oldColor != newColor)
+            dfs(image, sr, sc, newColor, oldColor);
         return image;
     }
 
-    private void dfs(int[][] image, boolean[][] visited, int i, int j, int newColor, int oldColor) {
+    public void dfs(int[][] image, int i, int j, int newColor, int oldColor) {
+        int height = image.length;
+        int weight = image[0].length;
+
+        if (i >= 0 && i < height && j >= 0 && j < weight) {
+
+            if (image[i][j] == oldColor) {
+                image[i][j] = newColor;
+
+                dfs(image, i + 1, j, newColor, oldColor);
+                dfs(image, i, j + 1, newColor, oldColor);
+                dfs(image, i - 1, j, newColor, oldColor);
+                dfs(image, i, j - 1, newColor, oldColor);
+            }
+
+        }
+
+    }
+
+    public void dfs(int[][] image, boolean[][] visited, int i, int j, int newColor, int oldColor) {
         int height = image.length;
         int weight = image[0].length;
 
@@ -30,5 +51,12 @@ public class FoodFill_733 {
 
         }
 
+    }
+
+    public static void main(String[] args) {
+        FoodFill_733 fill = new FoodFill_733();
+        int[][] image = { { 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 } };
+        // image[0]={1,1,1};
+        fill.floodFill(image, 1, 1, 2);
     }
 }
