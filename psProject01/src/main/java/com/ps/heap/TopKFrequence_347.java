ce92a85a6449c3d09365ec8f84502ac5c3f8bd3e
ej <joomn11@gmail.com>
2021-08-05 23:25:36 +0900

    topKFrequence - remove unnecessory code

--- psProject01/src/main/java/com/ps/heap/TopKFrequence_347.java
+++ psProject01/src/main/java/com/ps/heap/TopKFrequence_347.java
@@ -1,37 +1,33 @@
 package com.ps.heap;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Set;
 
 public class TopKFrequence_347 {
-    
+
     public static void main(String[] args) {
-        
-        int [] nums = {1,1,1,2,2,3};
-        int [] rst = TopKFrequence_347.topKFrequent(nums, 2);
+
+        int[] nums = { 1, 1, 1, 2, 2, 3 };
+        int[] rst = TopKFrequence_347.topKFrequent(nums, 2);
 
         for (int i : rst) {
             System.out.print(i + ", ");
         }
-        
+
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        Set<Integer> keyset = new HashSet<>();
-        for (int i :nums) {
-            map.put(i, map.getOrDefault(i, 0)+1);
-            keyset.add(i);
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        
-        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a,b)->b.getValue() - a.getValue());
-        queue.addAll(map.entrySet());
 
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        queue.addAll(map.entrySet());
+        
         int[] rst = new int[k];
-        for (int i = 0 ; i< k; i++) {
+        for (int i = 0; i < k; i++) {
             rst[i] = queue.poll().getKey();
         }
         return rst;
