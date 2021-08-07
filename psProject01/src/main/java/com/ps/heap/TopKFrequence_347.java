e200d114a5cf39b6acb431c02f75e2e892bc45f7
ej <joomn11@gmail.com>
2021-08-07 19:55:20 +0900

    add top k frequence ps

--- psProject01/src/main/java/com/ps/heap/TopKFrequence_347.java
+++ psProject01/src/main/java/com/ps/heap/TopKFrequence_347.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class TopKFrequence_347 {
-
     public static void main(String[] args) {
 
         int[] nums = { 1, 1, 1, 2, 2, 3 };
@@ -14,7 +13,6 @@ public class TopKFrequence_347 {
         for (int i : rst) {
             System.out.print(i + ", ");
         }
-
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
@@ -25,7 +23,32 @@ public class TopKFrequence_347 {
 
         PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
         queue.addAll(map.entrySet());
-        
+
+        int[] rst = new int[k];
+        for (int i = 0; i < k; i++) {
+            rst[i] = queue.poll().getKey();
+        }
+        return rst;
+    }
+
+    // 주어진 k 길이 만큼만 오름차순 queue를 구성하도록, queue의 길이가 k보다 길어지면 poll 하여
+    // 마지막에는 k개의 요소만 남으므로 해당 값들을 배열로 구성해서 리턴
+    public static int[] topKFrequent_min(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+        for (Map.Entry<Integer, Integer> m : map.entrySet()) {
+            queue.add(m);
+
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+
         int[] rst = new int[k];
         for (int i = 0; i < k; i++) {
             rst[i] = queue.poll().getKey();
