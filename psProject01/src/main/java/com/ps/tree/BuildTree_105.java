0c5186583713000f1143480b217698ca9d8f850a
ej <joomn11@gmail.com>
2021-09-02 23:43:38 +0900

    add build tree ps - 105

---
+++ psProject01/src/main/java/com/ps/tree/BuildTree_105.java
@@ -0,0 +1,13 @@
+package com.ps.tree;
+
+import com.ps.base.TreeNode;
+
+public class BuildTree_105 {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        TreeNode root = new TreeNode(preorder[0]);
+
+        return root;
+    }
+}
