0758187f5560f3b06ba9a3923b044d9fd8c174b2
ej <joomn11@gmail.com>
2021-09-07 23:43:31 +0900

    add queue 622 ps - template class

---
+++ psProject01/src/main/java/com/ps/queue/MyCircularQueue_622.java
@@ -0,0 +1,34 @@
+package com.ps.queue;
+
+public class MyCircularQueue_622 {
+
+    public MyCircularQueue_622(int k) {
+
+    }
+
+    public boolean enQueue(int value) {
+        return true;
+    }
+
+    public boolean deQueue() {
+
+        return true;
+    }
+
+    public int Front() {
+        return 0;
+    }
+
+    public int Rear() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public boolean isFull() {
+        return true;
+    }
+
+}
