2a7ca71749724ead19004650e93a606eddecc60f
ej <joomn11@gmail.com>
2021-12-07 23:39:32 +0900

    modify ps Array - binary search 704

--- psProject01/src/main/java/com/ps/array/BinarySearch_704.java
+++ psProject01/src/main/java/com/ps/array/BinarySearch_704.java
@@ -4,6 +4,21 @@ public class BinarySearch_704 {
 
     public int search(int[] nums, int target) {
 
-        return 0;
+        int left = 0;
+        int right = nums.length - 1;
+        int pivot = 0;
+
+        while (left <= right) {
+            pivot = (left + right) / 2;
+
+            if (target == nums[pivot]) {
+                return pivot;
+            } else if (nums[pivot] < target) {
+                left = pivot + 1;
+            } else {
+                right = pivot - 1;
+            }
+        }
+        return -1;
     }
 }
