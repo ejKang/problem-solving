e64091f558b67d2304bddc3c880ca55206ae2b8b
ej <joomn11@gmail.com>
2021-12-21 14:47:24 +0900

    add ps Array - find peak element

---
+++ psProject01/src/main/java/com/ps/array/FindPeakElement_162.java
@@ -0,0 +1,20 @@
+package com.ps.array;
+
+public class FindPeakElement_162 {
+
+    public int findPeakElement(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (right > left) {
+            int pivot = (left + right) / 2;
+            if (nums[pivot] < nums[pivot + 1]) {
+                left = pivot + 1;
+            } else {
+                right = pivot;
+            }
+        }
+        return right;
+    }
+}
