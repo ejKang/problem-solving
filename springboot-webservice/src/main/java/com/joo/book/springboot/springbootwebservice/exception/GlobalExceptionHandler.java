bd519e3d40817910d109577116c937f4b433649f
ej <joomn11@gmail.com>
2021-12-28 20:24:21 +0900

    controlleradvice test - without responseEntity

--- springboot-webservice/src/main/java/com/joo/book/springboot/springbootwebservice/exception/GlobalExceptionHandler.java
+++ springboot-webservice/src/main/java/com/joo/book/springboot/springbootwebservice/exception/GlobalExceptionHandler.java
@@ -6,15 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(IllegalArgumentException.class)
-	protected ResponseEntity<?> handleIllegalArguException(IllegalArgumentException e) {
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	protected ResponseError handleIllegalArguException(IllegalArgumentException e) {
 
-		return new ResponseEntity<>(
-				new ResponseError().setDetails(e.getLocalizedMessage()).setTimestamp(LocalDateTime.now()),
-				HttpStatus.INTERNAL_SERVER_ERROR);
+//		return new ResponseEntity<>(
+//				new ResponseError().setDetails(e.getLocalizedMessage()).setTimestamp(LocalDateTime.now()),
+//				HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseError().setDetails(e.getLocalizedMessage()).setTimestamp(LocalDateTime.now());
 	}
 }
