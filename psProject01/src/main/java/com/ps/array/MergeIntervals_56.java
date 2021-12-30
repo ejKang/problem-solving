f0c1b6a8dd6d822034f7616bb7d8fcca191a812d
ej <joomn11@gmail.com>
2021-12-30 23:54:29 +0900

    update ps Array - merge intervals 56

--- psProject01/src/main/java/com/ps/array/MergeIntervals_56.java
+++ psProject01/src/main/java/com/ps/array/MergeIntervals_56.java
@@ -5,12 +5,8 @@ import java.util.Arrays;
 public class MergeIntervals_56 {
     public int[][] merge(int[][] intervals) {
 
-        // sorting o(nlogn)
+        // sorting - time complex - o(nlogn)
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
-        for (int[] a : intervals) {
-            System.out.println(a[0] + " " + a[1]);
-        }
 
         int[][] rst = new int[intervals.length][2];
         rst[0] = intervals[0];
@@ -24,13 +20,8 @@ public class MergeIntervals_56 {
             }
         }
 
-        for (int[] a : rst) {
-            System.out.println(a[0] + " " + a[1]);
-        }
-
-        int[][] r = new int[j+1][2];
-
-        for (int i = 0; i < j+1; i++) {
+        int[][] r = new int[j + 1][2];
+        for (int i = 0; i < j + 1; i++) {
             r[i] = rst[i];
         }
 
