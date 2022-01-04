0ebe71ba868816d562a8c07e2f09b887f9b9728d
ej <joomn11@gmail.com>
2022-01-04 19:52:07 +0900

    Update SecurityConfig.java

--- springboot-webservice/src/main/java/com/joo/book/springboot/springbootwebservice/config/auth/SecurityConfig.java
+++ springboot-webservice/src/main/java/com/joo/book/springboot/springbootwebservice/config/auth/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	/*
         http.csrf().disable().headers().frameOptions().disable() // h2-console 화면을 사용하기 위해
                 .and().authorizeRequests() // url별 권한 관리를 설정하는 옵션의 시작점
                 .antMatchers("/", "/css/**", "images/**", "/js/**", "h2-console/**", "/hello/dto").permitAll() // 권한관리대상을
@@ -29,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .userInfoEndpoint() // oauth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들
                 .userService(customOAuth2UserService); // 로그인 성공 시 후속 조치를 진행할 userservice 구현체를 등록
                                                        // 리소스 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시
-
+*/
+    	http.csrf().disable().headers().frameOptions().disable().and().authorizeRequests().anyRequest().permitAll();
     }
 
 }
