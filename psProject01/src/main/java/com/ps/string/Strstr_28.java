12ce2dde8bf191512a21db41615ef59fc8d3d795
ej <joomn11@gmail.com>
2022-01-27 22:38:58 +0900

    add ps String - implement strStr() 28

--- psProject01/src/main/java/com/ps/string/Strstr_28.java
+++ psProject01/src/main/java/com/ps/string/Strstr_28.java
@@ -1,7 +1,23 @@
+package com.ps.string;
+
 public class Strstr_28 {
 
     public int strStr(String haystack, String needle) {
-        
-        return 0;   
+
+        int needleLen = needle.length();
+
+        if (needleLen == 0) {
+            return 0;
+        }
+
+        String substr = null;
+        for (int i = 0; i <= haystack.length() - needleLen; i++) {
+            substr = haystack.substring(i, i + needleLen);
+
+            if (substr.equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
