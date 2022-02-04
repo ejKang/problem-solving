f174f2c4487e99413dd71891023beb8552027acc
ej <joomn11@gmail.com>
2022-02-04 17:33:23 +0900

    review ps Array - 56

--- psProject01/src/main/java/com/ps/array/MergeIntervals_56.java
+++ psProject01/src/main/java/com/ps/array/MergeIntervals_56.java
@@ -27,4 +27,27 @@ public class MergeIntervals_56 {
 
         return r;
     }
+
+    public int[][] merge_2(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int[][] tmp = new int[intervals.length][2];
+        tmp[0] = intervals[0];
+        int tmpIdx = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (tmp[tmpIdx][1] >= intervals[i][0]) {
+                tmp[tmpIdx][1] = Math.max(tmp[tmpIdx][1], intervals[i][1]);
+            } else {
+                tmp[++tmpIdx] = intervals[i];
+            }
+        }
+
+        int[][] rst = new int[++tmpIdx][2];
+
+        for (int i = 0; i < tmpIdx; i++) {
+            rst[i] = tmp[i];
+        }
+        return rst;
+    }
 }
