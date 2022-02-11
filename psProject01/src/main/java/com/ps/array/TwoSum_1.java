9752824d55811cc0bb908037c8dba0ab73f83338
ej <joomn11@gmail.com>
2022-02-11 14:43:45 +0900

    review ps Array - 1

--- psProject01/src/main/java/com/ps/array/TwoSum_1.java
+++ psProject01/src/main/java/com/ps/array/TwoSum_1.java
@@ -20,4 +20,30 @@ public class TwoSum_1 {
         }
         return null;
     }
+
+    public int[] twoSum_2(int[] nums, int target) {
+
+        // for (int i = 0; i < nums.length - 1; i++) {
+        // int num1 = nums[i];
+        // for (int j = i + 1; j < nums.length; j++) {
+
+        // if (target - num1 == nums[j]) {
+        // return new int[] { i, j };
+        // }
+        // }
+        // }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int tmp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            tmp = target - nums[i];
+            if (map.containsKey(tmp)) {
+                return new int[] { i, map.get(tmp) };
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
 } // 15, 259, 16, 18
