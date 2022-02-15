27c4e799d1578e5f721fdf089fb6d72f84fc5bd3
ej <joomn11@gmail.com>
2022-02-15 17:42:25 +0900

    review ps String - 28

--- psProject01/src/main/java/com/ps/string/Strstr_28.java
+++ psProject01/src/main/java/com/ps/string/Strstr_28.java
@@ -21,4 +21,45 @@ public class Strstr_28 {
         }
         return -1;
     }
+
+    public int strStr_2(String haystack, String needle) {
+
+        int power = 1;
+        int nHash = 0;
+        int hHash = 0;
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+
+            if (i == 0) {
+                for (int j = needle.length() - 1; j >= 0; j--) {
+                    nHash = nHash + power * (needle.charAt(j) - 'a');
+                    hHash = hHash + power * (haystack.charAt(j) - 'a');
+
+                    if (j != 0) {
+                        power *= 2;
+                    }
+
+                }
+            } else {
+                hHash = (hHash - (haystack.charAt(i - 1) - 'a') * power) * 2
+                        + (haystack.charAt(i + needle.length() - 1) - 'a');
+            }
+
+            if (nHash == hHash) {
+                boolean found = true;
+                int k = 0;
+                for (int j = i; j < i + needle.length(); j++) {
+                    if (haystack.charAt(j) != needle.charAt(k)) {
+                        found = false;
+                        break;
+                    }
+                    k++;
+                }
+                if (found) {
+                    return i;
+                }
+            }
+
+        }
+        return -1;
+    }
 }
