ad878be05709c8fbded21052a20184cc44353379
ej <joomn11@gmail.com>
2022-03-08 18:45:47 +0900

    review ps DynamicProgramming - 322

--- psProject01/src/main/java/com/ps/dp/CoinChange_322.java
+++ psProject01/src/main/java/com/ps/dp/CoinChange_322.java
@@ -4,7 +4,8 @@ public class CoinChange_322 {
     public static void main(String[] args) {
         CoinChange_322 c = new CoinChange_322();
         int[] coins = { 2 };
-        c.coinChange(coins, 3);
+        // c.coinChange(coins, 3);
+        c.coinChange_2(coins, 3);
     }
 
     public int coinChange(int[] coins, int amount) {
@@ -70,5 +71,75 @@ public class CoinChange_322 {
             }
         }
         return cnt[amount];
+    }
+
+    public int coinChange_2(int[] coins, int amount) {
+        int[] rst = new int[amount + 1];
+
+        for (int i : coins) {
+            rst[i] = 1;
+        }
+
+        dp_2_top_down(coins, rst, amount);
+
+        if (rst[amount] == 0) {
+            return -1;
+        }
+        return rst[amount];
+    }
+
+    private int dp_2_top_down(int[] coins, int[] rst, int amount) {
+
+        if (amount == 0) {
+            return 0;
+        } else if (amount < 0) {
+            return -1;
+        } else if (rst[amount] != 0) {
+            return rst[amount];
+        }
+
+        int tmp = Integer.MAX_VALUE;
+
+        for (int coin : coins) {
+            int t = dp_2_top_down(coins, rst, amount - coin);
+
+            if (t != -1 && t < tmp) {
+                tmp = t + 1;
+            }
+        }
+
+        if (tmp != Integer.MAX_VALUE) {
+            rst[amount] = tmp;
+        } else {
+            rst[amount] = -1;
+            tmp = -1;
+        }
+        return tmp;
+    }
+
+    public int coinChange_2_bottom_up(int[] coins, int amount) {
+        int rst[] = new int[amount + 1];
+
+        for (int i = 1; i < amount + 1; i++) {
+            int min = Integer.MAX_VALUE;
+
+            for (int coin : coins) {
+
+                if (i - coin < 0) {
+                    continue;
+                } else if (i - coin == 0) {
+                    min = 0;
+                } else if (i - coin > 0) {
+                    min = Math.min(min, rst[i - coin]);
+                }
+            }
+
+            if (min == Integer.MAX_VALUE) {
+                rst[i] = -1;
+            } else {
+                rst[i] = min + 1;
+            }
+        }
+        return rst[amount];
     }
 }
