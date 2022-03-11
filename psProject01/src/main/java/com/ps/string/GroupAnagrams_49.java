ce9a132975f53daa1837122399ae763f37859dfd
ej <joomn11@gmail.com>
2022-03-11 15:37:16 +0900

    review ps String - 49

--- psProject01/src/main/java/com/ps/string/GroupAnagrams_49.java
+++ psProject01/src/main/java/com/ps/string/GroupAnagrams_49.java
@@ -8,6 +8,12 @@ import java.util.Map;
 
 public class GroupAnagrams_49 {
 
+    public static void main(String[] args) {
+        GroupAnagrams_49 g = new GroupAnagrams_49();
+        String[] ss = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        System.out.println(g.groupAnagrams_2_no_sort(ss));
+    }
+
     public List<List<String>> groupAnagrams(String[] strs) {
 
         Map<String, List<String>> map = new HashMap<>();
@@ -57,11 +63,55 @@ public class GroupAnagrams_49 {
             }
 
             String key = sb.toString();
+            System.out.println(key);
             map.putIfAbsent(key, new ArrayList<>());
 
             map.get(key).add(str);
         }
 
+        return new ArrayList<>(map.values());
+    }
+
+    public List<List<String>> groupAnagrams_2(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+
+            String key = String.valueOf(arr);
+            map.putIfAbsent(key, new ArrayList<>());
+
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public List<List<String>> groupAnagrams_2_no_sort(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            int[] tmp = new int[26];
+
+            for (char c : str.toCharArray()) {
+                tmp[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < tmp.length; i++) {
+                if (tmp[i] != 0) {
+                    char cc = (char) ('a' + i);
+                    sb.append(cc).append(tmp[i]);
+                }
+            }
+            String s = sb.toString();
+            System.out.println("key: " + s);
+            map.putIfAbsent(s, new ArrayList<>());
+
+            map.get(s).add(str);
+        }
         return new ArrayList<>(map.values());
     }
 }
