c3e64b12dd92f0c996bf40c4e79496a4f521fdc8
ej <joomn11@gmail.com>
2022-03-21 20:14:26 +0900

    add ps Tree - 543

--- psProject01/src/main/java/com/ps/tree/DiameterOfBT_543.java
+++ psProject01/src/main/java/com/ps/tree/DiameterOfBT_543.java
@@ -48,4 +48,41 @@ public class DiameterOfBT_543 {
 
         return Math.max(right, left) + 1;
     }
+
+    public int max_2 = 0;
+
+    public int diameterOfBinaryTree_3(TreeNode root) {
+
+        recursive_2(root);
+        return max_2;
+    }
+
+    public static void main(String[] args) {
+        DiameterOfBT_543 d = new DiameterOfBT_543();
+        TreeNode node = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        node.left = node2;
+        d.recursive_2(node);
+        System.out.println(d.max_2);
+    }
+
+    private int recursive_2(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int r = 0;
+        if (node.right != null) {
+            r = recursive_2(node.right) + 1;
+        }
+        int l = 0;
+        if (node.left != null) {
+            l = recursive_2(node.left) + 1;
+        }
+
+        int tmp = Math.max(r, l);
+        max_2 = Math.max(max_2, l + r);
+        return tmp;
+    }
 }
