2ca64d97cdfd645a2b9bf3c1614da6df69fd6edc
ej <joomn11@gmail.com>
2022-03-29 23:45:30 +0900

    add ps DynamicProgramming - 139 (?)

--- psProject01/src/main/java/com/ps/dp/WordBreak_139.java
+++ psProject01/src/main/java/com/ps/dp/WordBreak_139.java
@@ -4,6 +4,23 @@ import java.util.List;
 
 public class WordBreak_139 {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return true;
+        boolean[] arr = new boolean[s.length() + 1];
+
+        arr[0] = true;
+
+        for (int i = 1; i < +s.length(); i++) {
+            for (String str : wordDict) {
+                if (str.length() <= i) {
+
+                    if (arr[i - str.length()]) {
+                        if (s.substring(i - str.length(), i).equals(str)) {
+                            arr[i] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return arr[s.length()];
     }
 }
