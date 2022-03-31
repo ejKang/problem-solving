77b14b5fab43ef5f8af6014dc703095907e36f9c
ej <joomn11@gmail.com>
2022-03-31 23:38:28 +0900

    union - other find 1319

--- psProject01/src/main/java/com/ps/graph/NumOperToNetworkConnect_1319.java
+++ psProject01/src/main/java/com/ps/graph/NumOperToNetworkConnect_1319.java
@@ -1,12 +1,14 @@
 package com.ps.graph;
 
-import java.util.Arrays;
-
 public class NumOperToNetworkConnect_1319 {
     public int makeConnected(int n, int[][] connections) {
         int[] parent = new int[n];
         int loopCnt = 0;
         int nodeCnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
 
         for (int[] con : connections) {
             int aParent = find(parent, con[0]);
@@ -19,16 +21,28 @@ public class NumOperToNetworkConnect_1319 {
             }
         }
 
-        nodeCnt = (int) Arrays.stream(parent).filter(i -> i == 0).count();
+        // nodeCnt = (int) Arrays.stream(parent).filter(i -> i == 0).count();
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                nodeCnt++;
+            }
+        }
         if (loopCnt >= nodeCnt - 1) {
             return nodeCnt - 1;
         }
         return -1;
     }
 
+    // private int find(int[] parent, int i) {
+
+    // if (parent[i] == 0) {
+    // return i;
+    // }
+    // return find(parent, parent[i]);
+    // }
     private int find(int[] parent, int i) {
 
-        if (parent[i] == 0) {
+        if (parent[i] == i) {
             return i;
         }
         return find(parent, parent[i]);
