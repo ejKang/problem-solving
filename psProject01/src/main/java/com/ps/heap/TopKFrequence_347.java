1ed804de7734a41c8b51256a2f94fb6feb21e1ee
ej <joomn11@gmail.com>
2022-04-05 19:24:58 +0900

    review ps Heap - 347

--- psProject01/src/main/java/com/ps/heap/TopKFrequence_347.java
+++ psProject01/src/main/java/com/ps/heap/TopKFrequence_347.java
@@ -80,4 +80,20 @@ public class TopKFrequence_347 { // 692, 973
         }
         return rst;
     }
+
+    public int[] topKFrequent_3(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        queue.addAll(map.entrySet());
+        int[] rst = new int[k];
+        for (int i = 0; i < k; i++) {
+            rst[i] = queue.poll().getKey();
+        }
+        return rst;
+    }
 }
