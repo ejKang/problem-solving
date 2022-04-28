d05d43b40f33ac4d00a8044716dcb8cf81367868
ej <joomn11@gmail.com>
2022-04-28 23:48:46 +0900

    review bt - 90

--- psProject01/src/main/java/com/ps/bt/SubSets2_90.java
+++ psProject01/src/main/java/com/ps/bt/SubSets2_90.java
@@ -28,4 +28,27 @@ public class SubSets2_90 {
             tmp.remove(tmp.size() - 1);
         }
     }
+
+    public List<List<Integer>> subsetsWithDup_2(int[] nums) {
+        List<List<Integer>> rst = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+        Arrays.sort(nums);
+        backtracking2(rst, list, nums, 0);
+        return rst;
+    }
+
+    private void backtracking2(List<List<Integer>> rst, List<Integer> list, int[] nums, int idx) {
+
+        rst.add(new ArrayList<>(list));
+
+        for (int i = idx; i < nums.length; i++) {
+            if (i == idx || nums[i-1] != nums[i]) {
+                list.add(nums[i]);
+                backtracking2(rst, list, nums, i+1);
+                list.remove(list.size()-1);
+            } 
+        }
+    }
+
 }
